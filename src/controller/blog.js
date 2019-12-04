4f2d5f1c8f7d80c13e51cda768cc9d5abcd8bf3a
yzj_jimson <jimson@yzjtech.com>
2019-12-04 20:16:19 +0800

    添加假数据

--- src/controller/blog.js
+++ src/controller/blog.js
@@ -17,6 +17,38 @@ const getList = (author, keyword) => {
     }
   ]
 }
+
+const getDetail = (id) => {
+  return {
+    id: 1,
+    title: '标题A',
+    content: '内容A',
+    createTime: 1575372992187,
+    author: 'jimson'
+  }
+}
+
+const newBlog = (blogData = {}) => {
+  //blogData 是一个博客对象，包含title content 属性
+  return {
+    id: 3
+  }
+}
+
+const updateBlog = (id, blogData = {}) => {
+  // id 就是要更新博客的id
+  // blogData 是一个博客对象，包含title content 属性
+  return true
+}
+
+const delBlog = (id) => {
+  // id 就是要删除博客的id
+  return true
+}
 module.exports = {
-  getList
+  getList,
+  getDetail,
+  newBlog,
+  updateBlog,
+  delBlog
 }
